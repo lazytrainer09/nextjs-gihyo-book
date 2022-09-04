785e46c4a717592edd696dd0e8b957035d5ade94
lazytrainer09 <wihan0927@gmail.com>
2022-09-04 15:15:16 +0900

    section3

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,6 +1,9 @@
+// 商品カテゴリ
 export type Category = 'shoes' | 'clothes' | 'book'
+// 商品の状態
 export type Condition = 'new' | 'used'
 
+// ユーザー
 export type User = {
   id: number
   username: string
@@ -10,6 +13,7 @@ export type User = {
   description: string
 }
 
+// 商品
 export type Product = {
   id: number
   category: Category
@@ -22,6 +26,7 @@ export type Product = {
   owner: User
 }
 
+// APIコンテキスト
 export type ApiContext = {
   apiRootUrl: string
 }
