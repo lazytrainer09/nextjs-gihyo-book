18bdc786820508b209b443ab1ca46622220c39cc
lazytrainer09 <wihan0927@gmail.com>
2022-09-04 21:39:07 +0900

    section6

--- src/components/layout/Flex/index.tsx
+++ src/components/layout/Flex/index.tsx
@@ -1,54 +1,55 @@
 /* eslint-disable prettier/prettier */
 import styled from 'styled-components'
-import type { Responsive } from 'types/styles'
-import { toPropValue, Color, Space } from 'utils/styles'
+import Box, { BoxProps } from 'components/layout/Box'
+import type {
+  Responsive,
+  CSSPropertyAlignItems,
+  CSSPropertyAlignContent,
+  CSSPropertyJustifyContent,
+  CSSPropertyJustifyItems,
+  CSSPropertyFlexDirection,
+  CSSPropertyJustifySelf,
+  CSSPropertyFlexWrap,
+  CSSPropertyAlignSelf,
+} from 'types/styles'
+import { toPropValue } from 'utils/styles'
 
-export type BoxProps = {
-  color?: Responsive<Color>
-  backgroundColor?: Responsive<Color>
-  width?: Responsive<string>
-  height?: Responsive<string>
-  minWidth?: Responsive<string>
-  minHeight?: Responsive<string>
-  display?: Responsive<string>
-  border?: Responsive<string>
-  overflow?: Responsive<string>
-  margin?: Responsive<Space>
-  marginTop?: Responsive<Space>
-  marginRight?: Responsive<Space>
-  marginBottom?: Responsive<Space>
-  marginLeft?: Responsive<Space>
-  padding?: Responsive<Space>
-  paddingTop?: Responsive<Space>
-  paddingRight?: Responsive<Space>
-  paddingBottom?: Responsive<Space>
-  paddingLeft?: Responsive<Space>
+type FlexProps = BoxProps & {
+  alignItems?: Responsive<CSSPropertyAlignItems>
+  alignContent?: Responsive<CSSPropertyAlignContent>
+  justifyContent?: Responsive<CSSPropertyJustifyContent>
+  justifyItems?: Responsive<CSSPropertyJustifyItems>
+  flexWrap?: Responsive<CSSPropertyFlexWrap>
+  flexBasis?: Responsive<string>
+  flexDirection?: Responsive<CSSPropertyFlexDirection>
+  flexGrow?: Responsive<string>
+  flexShrink?: Responsive<string>
+  justifySelf?: Responsive<CSSPropertyJustifySelf>
+  alignSelf?: Responsive<CSSPropertyAlignSelf>
+  order?: Responsive<string>
 }
 
 /**
- * Boxコンポーネント
- * レイアウトの調整に利用する
+ * Flexコンポーネント
+ * flexboxの実現に利用する
  */
-const Box = styled.div<BoxProps>`
-  ${(props) => toPropValue('color', props.color, props.theme)}
-  ${(props) => toPropValue('background-color', props.backgroundColor, props.theme)}
-  ${(props) => toPropValue('width', props.width, props.theme)}
-  ${(props) => toPropValue('height', props.height, props.theme)}
-  ${(props) => toPropValue('min-width', props.minWidth, props.theme)}
-  ${(props) => toPropValue('min-height', props.minHeight, props.theme)}
-  ${(props) => toPropValue('display', props.display, props.theme)}
-  ${(props) => toPropValue('border', props.border, props.theme)}
-  ${(props) => toPropValue('overflow', props.overflow, props.theme)}
-  ${(props) => toPropValue('margin', props.margin, props.theme)}
-  ${(props) => toPropValue('margin-top', props.marginTop, props.theme)}
-  ${(props) => toPropValue('margin-left', props.marginLeft, props.theme)}
-  ${(props) => toPropValue('margin-bottom', props.marginBottom, props.theme)}
-  ${(props) => toPropValue('margin-right', props.marginRight, props.theme)}
-  ${(props) => toPropValue('padding', props.padding, props.theme)}
-  ${(props) => toPropValue('padding-top', props.paddingTop, props.theme)}
-  ${(props) => toPropValue('padding-left', props.paddingLeft, props.theme)}
-  ${(props) => toPropValue('padding-bottom', props.paddingBottom, props.theme)}
-  ${(props) => toPropValue('padding-right', props.paddingRight, props.theme)}
+const Flex = styled(Box)<FlexProps>`
+  ${(props) => toPropValue('align-items', props.alignItems, props.theme)}
+  ${(props) => toPropValue('align-content', props.alignContent, props.theme)}
+  ${(props) => toPropValue('justify-content', props.justifyContent, props.theme)}
+  ${(props) => toPropValue('justify-items', props.justifyItems, props.theme)}
+  ${(props) => toPropValue('flex-wrap', props.flexWrap, props.theme)}
+  ${(props) => toPropValue('flex-basis', props.flexBasis, props.theme)}
+  ${(props) => toPropValue('flex-direction', props.flexDirection, props.theme)}
+  ${(props) => toPropValue('flex-grow', props.flexGrow, props.theme)}
+  ${(props) => toPropValue('flex-shrink', props.flexShrink, props.theme)}
+  ${(props) => toPropValue('justify-self', props.justifySelf, props.theme)}
+  ${(props) => toPropValue('align-self', props.alignSelf, props.theme)}
+  ${(props) => toPropValue('order', props.order, props.theme)}
 `
 
-export default Box
+Flex.defaultProps = {
+  display: 'flex',
+}
+
+export default Flex
